881a524d7e34741b96e7cb4eabfe931e554d1cd2
Ratchagarn Naewbuntad <ratchagarn@gmail.com>
2023-02-25 08:14:52 +0700

    Remove unused file

--- src/_mocks/model/types.ts
+++
@@ -1,6 +0,0 @@
-import { ENTITY_TYPE, PRIMARY_KEY } from '@mswjs/data'
-
-export type InferModelType<T extends { getAll: (...args: any) => any }> = Omit<
-  ReturnType<T['getAll']>[number],
-  typeof ENTITY_TYPE | typeof PRIMARY_KEY
->
