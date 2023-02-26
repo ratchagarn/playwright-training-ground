e0f5810f37d522e2cb7dae3c643a903e0d160503
Ratchagarn Naewbuntad <ratchagarn@gmail.com>
2023-02-26 21:13:57 +0700

    Optimize RenderByRequestStatus

--- src/components/RenderByRequestStatus.tsx
+++ src/components/RenderByRequestStatus.tsx
@@ -1,36 +1,29 @@
-import type { UseQueryResult } from '@tanstack/react-query'
-
+import { type ReactElement } from 'react'
+import { UseQueryResult } from '@tanstack/react-query'
 import { Alert, Loading } from 'components/Elements'
 
 interface RenderByRequestStatusProps {
   status: UseQueryResult['status']
   errorMessage?: string
-  onSuccess?: JSX.Element | null
-  onError?: JSX.Element | null
-  onLoading?: JSX.Element | null
+  onSuccess?: ReactElement | null
+  onError?: ReactElement | null
+  onLoading?: ReactElement | null
 }
 
 const RenderByRequestStatus = ({
   status,
-  errorMessage,
+  errorMessage = 'Cannot get data',
   onError,
   onSuccess,
-  onLoading,
+  onLoading = <Loading />,
 }: RenderByRequestStatusProps) => {
   switch (status) {
     case 'error':
-      return onError === undefined || errorMessage ? (
-        <Alert
-          type="error"
-          message={errorMessage ? errorMessage : 'Cannot get data'}
-        />
-      ) : (
-        onError
-      )
+      return onError ?? <Alert type="error" message={errorMessage} />
     case 'success':
       return onSuccess ?? null
     default:
-      return onLoading === undefined ? <Loading /> : onLoading
+      return onLoading
   }
 }
 
